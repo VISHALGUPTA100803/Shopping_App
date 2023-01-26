44a1e71ba59f0c8dfeccebae4daf8a9cd2eacc93
VISHALGUPTA100803 <96491059+VISHALGUPTA100803@users.noreply.github.com>
2023-01-26 19:47:52 +0530

    shopping app

---
+++ lib/Screens/product_overview_screen.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/material.dart';
+
+class ProductOverviewScreen extends StatelessWidget {
+  const ProductOverviewScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
